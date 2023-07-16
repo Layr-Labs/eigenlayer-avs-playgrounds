cce8d8cb05162b8943d78320a6542b51331a1967
Samuel Laferriere <samlaf92@gmail.com>
2023-07-15 22:26:50 -0700

    added comment in run-all to explain current limitation of our script (doesnt work well with 2 deployed avs)

--- run-all.sh
+++ run-all.sh
@@ -22,11 +22,17 @@ make staker-queue-withdrawal
 make staker-notify-service-about-withdrawal
 # no freaking clue why this helps but without this
 # anvil was getting in some weird infinite loop bug
-sleep 1
+sleep 2
 make advanceChainBy100Blocks
 make staker-complete-queued-withdrawal
 # make freeze-operators
+# Note: operator needs to deregister from avs before running this script a second time
+#       otherwise, the operator will stay registered and serving the previous avs,
+#       which will prevent it from completing his withdrawal
+#       This is because we only store one avs in script/output/5/playground_avs_deployment_output.json
+#       our scripts only permit notifying (updating stakes in) the latest deployed avs
+# make deregister-operators-with-avs
 
-# print operator status
-make print-operators-status
+# print operator and staker status
 make print-stakers-status
+make print-operators-status
